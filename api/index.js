0705f0400f798fbd259ce9c2b28fb11b2e0f08d1
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-21 16:48:01 -0600

    Server Ready

--- api/index.js
+++ api/index.js
@@ -7,3 +7,16 @@ conn.sync({ force: true }).then(() => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
+
+//ESTE CODIGO ALTERNATIVO PERMITE SINCRONIZAR LOS MODELOS
+//CON LA ESTRUCTURA DE LAS TABLAS EXISTENTES, SIN ELIMINAR Y
+//RECREAR TODAS LAS TABLAS CON CADA INICIO DEL SERVIDOR
+// const server = require("./src/app.js");
+// const { conn } = require("./src/db.js");
+
+// // Syncing all the models at once.
+// conn.sync({ alter: true }).then(() => {
+//   server.listen(3001, () => {
+//     console.log("%s listening at 3001"); // eslint-disable-line no-console
+//   });
+// });
