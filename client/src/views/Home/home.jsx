57d549ac28587ac134b0ca7a17ad381e145620de
Gustavo Valdez <gustavo.gevs@gmail.com>
2023-06-26 14:03:47 -0600

    Home Page

---
+++ client/src/views/Home/home.jsx
@@ -0,0 +1,13 @@
+import style from './home.module.css'
+import React from 'react';
+
+function Home() {
+  return (
+    <div className={style.home}>
+      <h1>Henry Videogames</h1>
+      <p>Esta es la home page</p>
+    </div>
+  );
+}
+
+export default Home;
